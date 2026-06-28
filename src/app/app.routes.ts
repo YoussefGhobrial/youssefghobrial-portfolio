bc6371820fb4532905f0edc5e94c16edaca4bce7
YoussefShafik1 <150313863+YoussefShafik1@users.noreply.github.com>
2026-06-28 03:48:48 +0300

    Add navbar, footer, home components & routes
    Introduce Navbar, Footer and Home components (templates, styles, and unit tests), add new title/portfolio images, and wire them into the app. Replaced the placeholder app.html with <app-navbar />, <router-outlet />, and <app-footer />. Updated app.ts to import Navbar and Footer, added Home to app.routes with redirects and wildcard handling, and updated index.html (title and favicon). Small cosmetic assets and component scaffolding to establish site layout and routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // 1. The Default Route: What loads when someone visits yoursite.com
+  {
+    path: '',
+    component: Home,
+    title: 'Home | Youssef Ghobrial',
+  },
+  //   // 2. The Specific Path: If someone types yoursite.com/home, it redirects to the clean default URL
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+  //   // 3. The Wildcard Route: Catches typos (e.g., yoursite.com/asdf) and safely sends them back to Home
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
